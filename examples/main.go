51cdefd93b20d19d4ceae62a39759faea2129ac8
arata <arata04962@gmail.com>
2020-03-31 00:07:16 +0900

    [update] Examples

--- examples/main.go
+++ examples/main.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	. "github.com/arata-nvm/poly/poly"
+	. "github.com/arata-nvm/poly/vecmath"
+	"math"
 )
 
 func main() {
-	d := NewDevice(256, 256)
-	d.ClearColorBuffer(BLACK)
-
 	m := LoadObj("examples/box3.obj")
+	m.Rotation = Unit().MulScalar(90 * math.Pi / 180)
+	m.Scale = Unit().MulScalar(0.5)
 
-	d.DrawMesh(m, WHITE)
+	size := 256
+
+	d := NewDevice(size, size)
+
+	c := NewCamera(NewVector3(2, 0, 10), Zero(), UnitY())
+	d.SetCamera(c)
+
+	d.Perspective(10, float64(size/size), 1, 10)
+
+	d.ClearColorBuffer(BLACK)
+	cl := NewColor(0.5, 1, 0.6, 1)
+	d.DrawMesh(m, cl)
 
 	Save("out.png", d.Image())
 }
