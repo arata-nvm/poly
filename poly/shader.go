a9d14978e7a82de3aad22e913aaaf187e537cc22
arata <arata04962@gmail.com>
2020-04-01 17:44:15 +0900

    [add] Texture shader

--- poly/shader.go
+++ poly/shader.go
@@ -49,3 +49,23 @@ func (s *FlatShader) Fragment(v Vertex, _ Vector3) Color {
 	f := clamp(v.Normal.Dot(s.Light), 0, 1)
 	return NewColor(s.Color.R*f, s.Color.G*f, s.Color.B*f, s.Color.A)
 }
+
+type TextureShader struct {
+	Texture *Texture
+}
+
+func NewTextureShader(texture *Texture) *TextureShader {
+	return &TextureShader{
+		Texture: texture,
+	}
+}
+
+func (s *TextureShader) Vertex(v Vertex, m Matrix4) Vertex {
+	v.Coordinates = TransformCoordinate(v.Coordinates, m)
+	v.Normal = TransformCoordinate(v.Normal, m).Normalize()
+	return v
+}
+
+func (s *TextureShader) Fragment(v Vertex, _ Vector3) Color {
+	return s.Texture.Map(v.Uv.X, v.Uv.Y)
+}
