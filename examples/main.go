ff4386a7b90574d03c6bc7c4c2c4fa0faf451d9e
arata <arata04962@gmail.com>
2020-04-01 15:02:56 +0900

    [update] Examples

--- examples/main.go
+++ examples/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	. "github.com/arata-nvm/poly/poly"
 	. "github.com/arata-nvm/poly/vecmath"
 	"math"
@@ -8,7 +9,7 @@ import (
 
 func main() {
 	m := LoadObj("examples/box3.obj")
-	m.Rotation = Unit().MulScalar(90 * math.Pi / 180)
+	m.Rotation = Unit().MulScalar(10 * math.Pi / 180)
 	m.Scale = Unit().MulScalar(0.5)
 
 	size := 256
@@ -21,10 +22,37 @@ func main() {
 	d.Perspective(10, float64(size/size), 1, 10)
 
 	cl := NewColor(0.5, 1, 0.6, 1)
-	d.SetShader(NewSolidShader(cl))
+	light := NewVector3(1, 1, 1)
+	d.SetShader(NewFlatShader(cl, light))
 
 	d.ClearColorBuffer(BLACK)
-	d.DrawMesh(m, cl)
+	d.DrawMesh(m)
 
 	Save("out.png", d.Image())
+}
+
+func angleTest() {
+	for a := 0; a < 90; a++ {
+		m := LoadObj("examples/sphere.obj")
+		m.Rotation = Unit().MulScalar(float64(a) * math.Pi / 180)
+		m.Scale = Unit().MulScalar(0.5)
+
+		size := 256
+
+		d := NewDevice(size, size)
+
+		c := NewCamera(NewVector3(2, 0, 10), Zero(), UnitY())
+		d.SetCamera(c)
+
+		d.Perspective(10, float64(size/size), 1, 10)
+
+		cl := NewColor(0.5, 1, 0.6, 1)
+		light := NewVector3(1, 1, 1)
+		d.SetShader(NewFlatShader(cl, light))
+
+		d.ClearColorBuffer(BLACK)
+		d.DrawMesh(m)
+
+		Save(fmt.Sprintf("out/out_%d.png", a), d.Image())
+	}
 }
