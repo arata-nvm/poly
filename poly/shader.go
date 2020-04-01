32f079b29058eebcd8a0e6608a8d08d077079f25
arata <arata04962@gmail.com>
2020-04-01 15:00:55 +0900

    [add] Flat shader

--- poly/shader.go
+++ poly/shader.go
@@ -1,6 +1,8 @@
 package poly
 
-import . "github.com/arata-nvm/poly/vecmath"
+import (
+	. "github.com/arata-nvm/poly/vecmath"
+)
 
 type Shader interface {
 	Vertex(Vertex, Matrix4) Vertex
@@ -23,4 +25,27 @@ func (s *SolidShader) Vertex(v Vertex, m Matrix4) Vertex {
 
 func (s *SolidShader) Fragment(_ Vertex, _ Vector3) Color {
 	return s.Color
+}
+
+type FlatShader struct {
+	Color Color
+	Light Vector3
+}
+
+func NewFlatShader(color Color, light Vector3) *FlatShader {
+	return &FlatShader{
+		Color: color,
+		Light: light.Normalize(),
+	}
+}
+
+func (s *FlatShader) Vertex(v Vertex, m Matrix4) Vertex {
+	v.Coordinates = TransformCoordinate(v.Coordinates, m)
+	v.Normal = TransformCoordinate(v.Normal, m).Normalize()
+	return v
+}
+
+func (s *FlatShader) Fragment(v Vertex, _ Vector3) Color {
+	f := clamp(v.Normal.Dot(s.Light), 0, 1)
+	return NewColor(s.Color.R*f, s.Color.G*f, s.Color.B*f, s.Color.A)
 }
