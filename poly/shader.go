7f774bd9db4a7fafe3ac4db784007f81a76fde28
arata <arata04962@gmail.com>
2020-04-02 21:18:04 +0900

    [add] Phong shader

--- poly/shader.go
+++ poly/shader.go
@@ -2,6 +2,7 @@ package poly
 
 import (
 	. "github.com/arata-nvm/poly/vecmath"
+	"math"
 )
 
 type Shader interface {
@@ -85,4 +86,39 @@ func (s *NormalShader) Vertex(v Vertex, m Matrix4) Vertex {
 func (s *NormalShader) Fragment(v Vertex, _ Vector3) Color {
 	n := v.Normal.Clamp(0, 1)
 	return NewColorFromVec(n)
+}
+
+type PhongShader struct {
+	Light Vector3
+	Eye   Vector3
+	Color Color
+	Pow   float64
+}
+
+func NewPhongShader(light, eye Vector3, color Color, pow float64) *PhongShader {
+	return &PhongShader{
+		Light: light.Normalize(),
+		Eye:   eye.Normalize(),
+		Color: color,
+		Pow:   pow,
+	}
+}
+
+func (s *PhongShader) Vertex(v Vertex, m Matrix4) Vertex {
+	v.Coordinates = TransformCoordinate(v.Coordinates, m)
+	v.Normal = TransformCoordinate(v.Normal, m).Normalize()
+	return v
+}
+
+func (s *PhongShader) Fragment(v Vertex, _ Vector3) Color {
+	half := s.Light.Add(s.Eye).Normalize()
+	diffuse := Clamp(v.Normal.Dot(s.Light), 0, 1)
+	specular := math.Pow(Clamp(v.Normal.Dot(half), 0, 1), s.Pow)
+	ambient := 0.05
+	return NewColor(
+		Clamp(s.Color.R*diffuse+specular+ambient, 0, 1),
+		Clamp(s.Color.G*diffuse+specular+ambient, 0, 1),
+		Clamp(s.Color.B*diffuse+specular+ambient, 0, 1),
+		s.Color.A,
+	)
 }
