e1d5acd14aa6924690d1ebe7996d895b11e5e1ef
arata <arata04962@gmail.com>
2020-04-02 20:59:56 +0900

    [add] Smoothing normal vectors

--- poly/mesh.go
+++ poly/mesh.go
@@ -17,3 +17,23 @@ func NewMesh() *Mesh {
 		Scale:    Unit(),
 	}
 }
+
+func (m *Mesh) SmoothNormals() {
+	// 同じ位置にある頂点の法線ベクトルの総和をとり、正規化
+	normals := make(map[Vector3]Vector3)
+	for _, f := range m.Faces {
+		normals[f.V1.Coordinates] = normals[f.V1.Coordinates].Add(f.V1.Normal)
+		normals[f.V2.Coordinates] = normals[f.V2.Coordinates].Add(f.V2.Normal)
+		normals[f.V3.Coordinates] = normals[f.V3.Coordinates].Add(f.V3.Normal)
+	}
+
+	for coord, normal := range normals {
+		normals[coord] = normal.Normalize()
+	}
+
+	for _, f := range m.Faces {
+		f.V1.Normal = normals[f.V1.Coordinates]
+		f.V2.Normal = normals[f.V2.Coordinates]
+		f.V3.Normal = normals[f.V3.Coordinates]
+	}
+}
